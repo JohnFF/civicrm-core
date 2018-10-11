24380467a17d3ba2b7fd2c1f8573f95d9e572a59
John Kirk <john.kirk@bedsafe.org>
2018-10-11 01:53:13 +0100

    1. Removed duplicate extensions if there's a remote and a local. 2. Made all installed extensions on tabs look like the install tab.

--- CRM/Admin/Page/Extensions.php
+++ CRM/Admin/Page/Extensions.php
@@ -144,15 +144,14 @@ class CRM_Admin_Page_Extensions extends CRM_Core_Page_Basic {
     // $manager->refresh();
 
     $localExtensionRows = $this->formatLocalExtensionRows();
-    usort($localExtensionRows, 'CRM_Admin_Page_Extensions::sortExtensionByStatus');
+    uasort($localExtensionRows, 'CRM_Admin_Page_Extensions::sortExtensionByStatus');
     $this->assign('localExtensionRows', $localExtensionRows);
 
     $remoteExtensionRows = $this->formatRemoteExtensionRows($localExtensionRows);
-    //$this->assign('remoteExtensionRows', $remoteExtensionRows);
+    $this->assign('remoteExtensionRows', $remoteExtensionRows);
 
     $extensionRows = array_replace($remoteExtensionRows, $localExtensionRows);
-    $this->assign('remoteExtensionRows', $remoteExtensionRows);
-    
+
     $this->categoriseExtensions($extensionRows);
     $this->sortExtensions($extensionRows);
     $this->assign('extensionsByCategory', $extensionRows);
@@ -355,7 +354,7 @@ class CRM_Admin_Page_Extensions extends CRM_Core_Page_Basic {
       case 'org.civicoop.postcodenl':
       case 'org.civicoop.areas':
       case 'com.aghstrategies.uscounties':
-        $category = 'Location';
+        $category = 'Geography';
         break;
 
       case 'org.wikimedia.rip':
@@ -381,7 +380,7 @@ class CRM_Admin_Page_Extensions extends CRM_Core_Page_Basic {
       case 'biz.jmaconsulting.olarkchat':
       case 'uk.co.vedaconsulting.gotowebinar':
       case 'org.civicoop.templateattachments':
-        $category = 'Communications';
+        $category = 'Communication';
         break;
 
       case 'org.civicrm.sms.clickatell':
@@ -518,10 +517,12 @@ class CRM_Admin_Page_Extensions extends CRM_Core_Page_Basic {
     // We then merge categories manually underneath.
     $extensionCategoryToTabMap['Developers'][] = 'APIs';
     unset ($extensionCategoryToTabMap['APIs']);
-    $extensionCategoryToTabMap['Communications'][] = 'Tokens';
+    $extensionCategoryToTabMap['Communication'][] = 'Tokens';
     unset ($extensionCategoryToTabMap['Tokens']);
-    $extensionCategoryToTabMap['Communications'][] = 'SMSProviders';
+    $extensionCategoryToTabMap['Communication'][] = 'SMSProviders';
     unset ($extensionCategoryToTabMap['SMSProviders']);
+    $extensionCategoryToTabMap['Utilities'][] = 'DataCleaning';
+    unset ($extensionCategoryToTabMap['DataCleaning']);
 
     $this->assign('extensionCategoryToTabMap', $extensionCategoryToTabMap);
   }
